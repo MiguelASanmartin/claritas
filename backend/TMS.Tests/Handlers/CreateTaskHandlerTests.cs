b50ef226e2b098c45872aed078b438e1f050faf8
MiguelASanmartin <miguelsd151@gmail.com>
2025-12-15 21:03:28 +0100

    Setup tests project with xUnit, Moq and FluentAssertions

---
+++ backend/TMS.Tests/Handlers/CreateTaskHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace TMS.Tests.Handlers
+{
+    public class CreateTaskHandlerTests
+    {
+        [Fact]
+        public void SmokeTest_ProjectCompiles()
+        {
+            var expected = true;
+
+            var actual = true;
+
+            actual.Should().Be(expected);
+        }
+    }
+}
